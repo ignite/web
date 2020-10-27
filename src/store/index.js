8960fc4475dec949654db106534ceb3906d559f1
vms <vince19972@gmail.com>
2020-10-27 17:08:40 -0400

    install required modules

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import Vuex from "vuex";
 import cosmos from "./cosmos";
 
+import blocks from './blocks'
+import txs from './txs'
+import env from './env'
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
