8a04c2cc9a0f9d39361135a35652cfba937b2c60
Brian <45702419+lukitsbrian@users.noreply.github.com>
2020-10-29 08:02:12 -0700

    SpTypeList component (#21)
    * Add SpTypeList component
    
    * Replace list in SpTypeForm with SpTypeList component
    
    * Apply suggestions from code review
    
    Co-authored-by: Vince MingPu Shao <vince19972@gmail.com>
    Co-authored-by: Denis Fadeev <denis@fadeev.org>

--- src/components/SpTypeForm.vue
+++ src/components/SpTypeForm.vue
@@ -19,20 +19,7 @@
           Create {{ type }}
         </sp-button>
       </div>
-      <sp-h3
-        >List of {{ type }} items {{ module && `in module ${module}` }}</sp-h3
-      >
-      <div class="item" v-for="instance in instanceList" :key="instance.id">
-        <div class="item__field" v-for="(value, key) in instance" :key="key">
-          <div class="item__field__key">{{ key }}:</div>
-          <div class="item__field__value">
-            {{ value }}
-          </div>
-        </div>
-      </div>
-      <div class="card__empty" v-if="instanceList.length < 1">
-        There are no {{ type }} items yet. Create one using the form.
-      </div>
+      <sp-type-list :type="type" :module="module"/>
     </div>
   </div>
 </template>
@@ -46,38 +33,6 @@
 .button {
   display: inline-block;
 }
-.item {
-  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
-  margin-bottom: 1rem;
-  padding: 1rem;
-  border-radius: 0.5rem;
-  overflow: hidden;
-}
-.item__field {
-  display: grid;
-  line-height: 1.5;
-  grid-template-columns: 15% 1fr;
-  grid-template-rows: 1fr;
-  word-break: break-all;
-}
-.item__field__key {
-  color: rgba(0, 0, 0, 0.25);
-  word-break: keep-all;
-  overflow: hidden;
-}
-.card__empty {
-  margin-bottom: 1rem;
-  border: 1px dashed rgba(0, 0, 0, 0.1);
-  padding: 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  border-radius: 8px;
-  color: rgba(0, 0, 0, 0.25);
-  text-align: center;
-  min-height: 8rem;
-}
 </style>
 
 <script>
@@ -85,6 +40,7 @@ import SpInput from "./SpInput";
 import IconLoading2 from "./IconLoading2";
 import SpH3 from "./SpH3";
 import SpButton from "./SpButton";
+import SpTypeList from "./SpTypeList";
 
 export default {
   props: {
@@ -108,6 +64,7 @@ export default {
     IconLoading2,
     SpH3,
     SpButton,
+    SpTypeList,
   },
   data: function() {
     return {
@@ -119,17 +76,10 @@ export default {
     (this.fields || []).forEach((field) => {
       this.$set(this.fieldsList, field, "");
     });
-    this.$store.dispatch("cosmos/entityFetch", {
-      type: this.type,
-      module: this.module,
-    });
   },
   computed: {
     hasAddress() {
       return !!this.$store.state.cosmos.account.address;
-    },
-    instanceList() {
-      return this.$store.state.cosmos.data[`${this.module}/${this.type}`] || [];
     },
     valid() {
       return Object.values(this.fieldsList).every((el) => {
