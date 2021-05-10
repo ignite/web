3963508f57168c228d57de935a0c2cca98deaa6a
Clockwork <alex@clockwork.gr>
2021-05-10 19:24:50 +0300

    feat: Welcome component

--- packages/vue/src/components/SpWelcome/SpWelcome.vue
+++ packages/vue/src/components/SpWelcome/SpWelcome.vue
@@ -1,15 +1,25 @@
 <template>
 	<div class="sp-component sp-welcome">
 		<div class="sp-box sp-shadow sp-welcome__main">
-			<h2>Welcome to Starport</h2>
+			<div class="sp-welcome__header">
+				<h2>Welcome to Starport</h2>
+			</div>
 			<p>
 				Explanation of what this front-end does and why etc.<br />
 				Explanation of what this front-end does and why etc.<br />
 				Explanation of what this front-end does and why etc.<br />
 				Explanation of what this front-end does and why etc.<br />
 			</p>
-			<SpButton href="https://docs.starport.network/" target="_blank" type="primary">Get started</SpButton>
-			<SpButton href="https://github.com/tendermint/starport" target="_blank" type="secondary">GitHub</SpButton>
+			<div class="sp-welcome__btns">
+				<SpButton href="https://docs.starport.network/" target="_blank" type="primary">Get started</SpButton>
+				<SpButton
+					href="https://github.com/tendermint/starport"
+					target="_blank"
+					type="secondary"
+					class="sp-welcome__btns__github"
+					>GitHub</SpButton
+				>
+			</div>
 		</div>
 	</div>
 </template>
