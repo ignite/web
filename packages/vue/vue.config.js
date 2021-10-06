f771c79d9d1fdf559c1a57ccb9a63f869076c37d
Clockwork <alex@clockwork.gr>
2021-10-06 18:40:14 +0300

    fix: package-locks and flow adjustments

--- packages/vue/vue.config.js
+++ packages/vue/vue.config.js
@@ -5,32 +5,32 @@ module.exports = {
   transpileDependencies: true,
   publicPath: './',
   configureWebpack: {
-		externals: {
-			'element-ui': {
-				commonjs: 'element-ui',
-				commonjs2: 'element-ui',
-				root: 'ELEMENT',
-				amd: 'element-ui',
-			},
-			vue: {
-				commonjs: 'vue',
-				commonjs2: 'vue',
-				root: 'Vue',
-				amd: 'vue',
-			},
-			axios: {
-				commonjs: 'axios',
-				commonjs2: 'axios',
-				root: 'axios',
-				amd: 'axios',
-			},
-			dayjs: {
-				commonjs: 'dayjs',
-				commonjs2: 'dayjs',
-				root: 'dayjs',
-				amd: 'dayjs',
-			},
-		},
+    externals: {
+      'element-ui': {
+        commonjs: 'element-ui',
+        commonjs2: 'element-ui',
+        root: 'ELEMENT',
+        amd: 'element-ui',
+      },
+      vue: {
+        commonjs: 'vue',
+        commonjs2: 'vue',
+        root: 'Vue',
+        amd: 'vue',
+      },
+      axios: {
+        commonjs: 'axios',
+        commonjs2: 'axios',
+        root: 'axios',
+        amd: 'axios',
+      },
+      dayjs: {
+        commonjs: 'dayjs',
+        commonjs2: 'dayjs',
+        root: 'dayjs',
+        amd: 'dayjs',
+      },
+    },
     entry: {
       main: './src/index.ts',
     },
