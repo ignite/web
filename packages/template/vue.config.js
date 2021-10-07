7d77bbd2a28e3ecddde53d029f5db841802dc875
Clockwork <alex@clockwork.gr>
2021-10-07 12:54:16 +0300

    fix: remove deprecated option

--- packages/template/vue.config.js
+++ packages/template/vue.config.js
@@ -2,9 +2,6 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
