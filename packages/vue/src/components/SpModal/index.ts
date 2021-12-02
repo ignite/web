304b7b5975b8d2517b79d1a810dac3059d58395b
Matthews <salamon.mat@gmail.com>
2021-12-02 23:48:28 +0000

    Spacing

--- packages/vue/src/components/SpModal/index.ts
+++ packages/vue/src/components/SpModal/index.ts
@@ -4,9 +4,9 @@ import SpModal from './SpModal.vue'
 import { registerComponent } from './../../utils/plugins/index'
 
 export const Plugin = {
-    install(vue: Application): void {
-        registerComponent(vue, SpModal)
-    },
+  install(vue: Application): void {
+    registerComponent(vue, SpModal)
+  },
 }
 
 export default SpButton
