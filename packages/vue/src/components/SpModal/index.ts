a63350c107f7fe1b50e2b5cfcb42fc2c7535aa92
Matthews <salamon.mat@gmail.com>
2021-12-02 23:45:01 +0000

    SpModal component

---
+++ packages/vue/src/components/SpModal/index.ts
@@ -0,0 +1,12 @@
+import { App as Application } from 'vue'
+import SpModal from './SpModal.vue'
+
+import { registerComponent } from './../../utils/plugins/index'
+
+export const Plugin = {
+    install(vue: Application): void {
+        registerComponent(vue, SpModal)
+    },
+}
+
+export default SpButton
