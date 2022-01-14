c739ccaf311b474cf4191decc628eb01f31328f9
marinhoarthur <arthurtutuca@gmail.com>
2022-01-14 14:56:17 -0300

    proper code styling

--- packages/vue/src/components/SpAssets/SpAssets.vue
+++ packages/vue/src/components/SpAssets/SpAssets.vue
@@ -1,75 +1,78 @@
 <template>
   <section>
-    <header class='assets-header'>
-      <h2 class='title'>Assets</h2>
+    <header class="assets-header">
+      <h2 class="title">Assets</h2>
     </header>
-    <table class='assets-table'>
+    <table class="assets-table">
       <tbody>
-      <tr v-for='balance in balances' :key='balance' class='assets-table__row'>
-        <td class='assets-table__denom'>
-          <div class="sp-denom-marker">
-            {{balance.denom.charAt(0).toUpperCase()}}
-          </div>
-          <div class='sp-denom-name'>
-            {{ balance.denom.toUpperCase() }}
-          </div>
-        </td>
-        <td class='assets-table__amount'>
-          {{ new Intl.NumberFormat('en-GB').format(balance.amount) }}
-        </td>
-      </tr>
+        <tr
+          v-for="balance in balances"
+          :key="balance"
+          class="assets-table__row"
+        >
+          <td class="assets-table__denom">
+            <div class="sp-denom-marker">
+              {{ balance.denom.charAt(0).toUpperCase() }}
+            </div>
+            <div class="sp-denom-name">
+              {{ balance.denom.toUpperCase() }}
+            </div>
+          </td>
+          <td class="assets-table__amount">
+            {{ new Intl.NumberFormat('en-GB').format(balance.amount) }}
+          </td>
+        </tr>
       </tbody>
     </table>
-
   </section>
 </template>
 
 <script lang="ts">
-/**
- * SpAssets Component
- *
- * This component displays the current account balance.
- */
-
 import { computed, defineComponent, onMounted, PropType } from 'vue'
-import { useStore } from "vuex"
+import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'SpAssets',
+
   props: {
     address: {
       type: String as PropType<string>
     }
   },
-  setup(props) {
-    // Access store via useStore hook.
-    const store = useStore()
 
-    onMounted(async () => {
-      await store.dispatch('cosmos.bank.v1beta1/QueryAllBalances', {
-        params: { address: props.address },
-        options: { all: true, subscribe: true },
-      })
-    })
+  setup(props) {
+    // store
+    const $s = useStore()
+
+    // actions
+    let queryAllBalances = (opts: any) =>
+      $s.dispatch('cosmos.bank.v1beta1/QueryAllBalances', opts)
 
     // computed
     let balances = computed(() => {
       return (
-        store.getters['cosmos.bank.v1beta1/getAllBalances']({
+        $s.getters['cosmos.bank.v1beta1/getAllBalances']({
           params: { address: props.address }
         })?.balances ?? []
       )
+    })
+
+    // lh
+    onMounted(async () => {
+      await queryAllBalances({
+        params: { address: props.address },
+        options: { all: true, subscribe: true }
+      })
     })
 
     return {
       balances
     }
   }
-
 })
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 .assets-header {
   display: flex;
   align-items: center;
@@ -80,8 +83,8 @@ export default defineComponent({
   width: 100%;
 
   &__denom {
-    border-top-left-radius: .75rem;
-    border-bottom-left-radius: .75rem;
+    border-top-left-radius: 0.75rem;
+    border-bottom-left-radius: 0.75rem;
     vertical-align: middle;
     padding-top: 1.25rem;
     padding-bottom: 1.25rem;
@@ -93,7 +96,7 @@ export default defineComponent({
     padding-bottom: 20px;
     padding-top: 20px;
 
-    font-family: Inter,serif;
+    font-family: Inter, serif;
     font-size: 16px;
     letter-spacing: -0.112px;
     line-height: 21px;
@@ -107,7 +110,7 @@ export default defineComponent({
 
 .sp-denom-name {
   display: inline-block;
-  font-family: Inter,serif;
+  font-family: Inter, serif;
   font-size: 16px;
   letter-spacing: -0.112px;
   line-height: 21px;
@@ -124,7 +127,12 @@ export default defineComponent({
   width: 24px;
   vertical-align: middle;
   margin-right: 0.8rem;
-  background: radial-gradient(83.33% 83.33% at 16.67% 16.67%, #F5F5F5 0%, #D7D7D7 42.19%, #FDFDFD 100%);
+  background: radial-gradient(
+    83.33% 83.33% at 16.67% 16.67%,
+    #f5f5f5 0%,
+    #d7d7d7 42.19%,
+    #fdfdfd 100%
+  );
   box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.62);
   border-radius: 24px;
   text-align: center;
@@ -141,7 +149,7 @@ export default defineComponent({
 }
 
 .title {
-  font-family: Inter,serif;
+  font-family: Inter, serif;
   font-style: normal;
   font-weight: bold;
   font-size: 28px;
