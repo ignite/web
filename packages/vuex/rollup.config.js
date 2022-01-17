a8a595ac9c689c03dc9f91ea5c1562c36f5c6067
marinhoarthur <arthurtutuca@gmail.com>
2022-01-17 11:01:38 -0300

    code:format

--- packages/vuex/rollup.config.js
+++ packages/vuex/rollup.config.js
@@ -2,10 +2,10 @@ import cjs from '@rollup/plugin-commonjs'
 import ts from '@rollup/plugin-typescript'
 
 export default {
-	input: 'src/index.js',
-	output: {
-		dir: 'lib',
-		format: 'esm'
-	},
-	plugins: [ts(), cjs()]
+  input: 'src/index.js',
+  output: {
+    dir: 'lib',
+    format: 'esm'
+  },
+  plugins: [ts(), cjs()]
 }
