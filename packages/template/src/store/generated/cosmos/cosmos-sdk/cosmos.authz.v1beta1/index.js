ddbdcf9cc481de3bc4b5308d26b2b7bfbe2bf360
marinhoarthur <arthurtutuca@gmail.com>
2022-01-24 08:43:49 -0300

    regenerated vuex

--- packages/template/src/store/generated/cosmos/cosmos-sdk/cosmos.authz.v1beta1/index.js
+++ packages/template/src/store/generated/cosmos/cosmos-sdk/cosmos.authz.v1beta1/index.js
@@ -129,23 +129,6 @@ export default {
                 throw new SpVuexError('QueryClient:QueryGrants', 'API Node Unavailable. Could not perform query: ' + e.message);
             }
         },
-        async sendMsgExec({ rootGetters }, { value, fee = [], memo = '' }) {
-            try {
-                const txClient = await initTxClient(rootGetters);
-                const msg = await txClient.msgExec(value);
-                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
-                        gas: "200000" }, memo });
-                return result;
-            }
-            catch (e) {
-                if (e == MissingWalletError) {
-                    throw new SpVuexError('TxClient:MsgExec:Init', 'Could not initialize signing client. Wallet is required.');
-                }
-                else {
-                    throw new SpVuexError('TxClient:MsgExec:Send', 'Could not broadcast Tx: ' + e.message);
-                }
-            }
-        },
         async sendMsgGrant({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
@@ -180,18 +163,20 @@ export default {
                 }
             }
         },
-        async MsgExec({ rootGetters }, { value }) {
+        async sendMsgExec({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
                 const msg = await txClient.msgExec(value);
-                return msg;
+                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
+                        gas: "200000" }, memo });
+                return result;
             }
             catch (e) {
                 if (e == MissingWalletError) {
                     throw new SpVuexError('TxClient:MsgExec:Init', 'Could not initialize signing client. Wallet is required.');
                 }
                 else {
-                    throw new SpVuexError('TxClient:MsgExec:Create', 'Could not create message: ' + e.message);
+                    throw new SpVuexError('TxClient:MsgExec:Send', 'Could not broadcast Tx: ' + e.message);
                 }
             }
         },
@@ -222,6 +207,21 @@ export default {
                 }
                 else {
                     throw new SpVuexError('TxClient:MsgRevoke:Create', 'Could not create message: ' + e.message);
+                }
+            }
+        },
+        async MsgExec({ rootGetters }, { value }) {
+            try {
+                const txClient = await initTxClient(rootGetters);
+                const msg = await txClient.msgExec(value);
+                return msg;
+            }
+            catch (e) {
+                if (e == MissingWalletError) {
+                    throw new SpVuexError('TxClient:MsgExec:Init', 'Could not initialize signing client. Wallet is required.');
+                }
+                else {
+                    throw new SpVuexError('TxClient:MsgExec:Create', 'Could not create message: ' + e.message);
                 }
             }
         },
