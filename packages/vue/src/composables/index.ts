0fd434bd1e9f82bd9d5b6b0fa50cb4de9abc4a0c
Arthur Marinho <arthurtutuca@gmail.com>
2022-01-24 13:46:52 -0300

    Update index.ts

--- packages/vue/src/composables/index.ts
+++ packages/vue/src/composables/index.ts
@@ -1,4 +1,4 @@
 export { default as useClipboard } from './useClipboard'
 export { default as useKeplr } from './useKeplr'
-export { default as usePagination } from './usePagination'
+export { default as useAPIPagination } from './useAPIPagination'
 export { default as useTxs } from './useTxs'
