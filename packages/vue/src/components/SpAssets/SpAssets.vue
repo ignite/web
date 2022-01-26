3630cd12e6dd2e67daaff6b5817a22562a93fbd6
Mihail Marcu <marku.michael@gmail.com>
2022-01-26 10:07:52 +0200

    #139: Add search bar for assets

--- packages/vue/src/components/SpAssets/SpAssets.vue
+++ packages/vue/src/components/SpAssets/SpAssets.vue
@@ -2,8 +2,19 @@
   <section>
     <header class="assets-header">
       <h2 class="title">Assets</h2>
+      <div class='assets-header__search'>
+        <div class='assets-header__search-content'>
+          <input type="text" autocomplete="off" placeholder="Search assets" class="input--search">
+        </div>
+      </div>
     </header>
     <table class="assets-table">
+      <thead class='assets-table__thead'>
+        <tr>
+          <td>Asset</td>
+          <td class='assets-table__align-right'>Available balance</td>
+        </tr>
+      </thead>
       <tbody>
         <tr
           v-for="balance in balances"
@@ -60,7 +71,8 @@ export default defineComponent({
     // lh
     onMounted(async () => {
       await queryAllBalances({
-        params: { address: props.address },
+        params: { address: props.address},
+        query: {"pagination.reverse": false},
         options: { all: true, subscribe: true }
       })
     })
@@ -75,8 +87,27 @@ export default defineComponent({
 <style lang="scss" scoped>
 .assets-header {
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 100%;
+  > * {
+    position: relative;
+    width: 100%;
+
+    &:first-child {
+      flex: 0 0 66.6666666667%;
+      max-width: 66.6666666667%;
+    }
+    &:last-child {
+      flex: 0 0 33.3333333333%;
+      max-width: 33.3333333333%;
+    }
+  }
+  &__search-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
 }
 
 .assets-table {
@@ -105,6 +136,16 @@ export default defineComponent({
     text-indent: 0;
     vertical-align: middle;
     font-weight: 700;
+  }
+  &__thead {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 153.8%;
+    color: rgba(0, 0, 0, 0.667);
+  }
+  &__align-right {
+    text-align: right;
   }
 }
 
@@ -160,5 +201,23 @@ export default defineComponent({
   font-feature-settings: 'zero';
 
   color: #000000;
+}
+
+.input {
+  &--search {
+    background-image: none;
+    border-radius: 4px;
+    border: rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+    color: rgba(0, 0, 0, 0.33);
+    display: inline-block;
+    font-size: 16px;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+    width: 100%;
+  }
 }
 </style>
