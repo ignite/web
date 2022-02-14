2fa70894ea0df859d3fe4f5314b31b4dcb00aa9e
Mihail Marcu <marku.michael@gmail.com>
2022-02-14 04:32:00 +0200

    Add skeleton loading for transaction list

--- packages/vue/src/components/SpTxList/SpTxList.vue
+++ packages/vue/src/components/SpTxList/SpTxList.vue
@@ -1,154 +1,42 @@
 <template>
-  <div class="tx-list">
-    <div class="title">Transactions</div>
+  <Suspense>
+    <template #default>
+      <SpGetTxList />
+    </template>
+    <template #fallback>
+      <div class="tx-list">
+        <div class="title">Transactions</div>
+        <div v-for='n in 4' :key='n' class='loading__row'>
+          <div class='loading__col'>
+            <span class='loading__avatar'></span>
+            <span class='loading__denom'></span>
+          </div>
 
-    <div v-if='newTxs' class="load-more" role="button" @click="loadNewItems">
-      <template v-if='state.isNewTxLoading'>
-        <SpSpinner size='16'></SpSpinner>
-      </template>
-      <template v-else>
-        {{ showMoreText }}
-      </template>
-    </div>
-    <div v-if="Object.keys(txByMonth).length > 0" class="list">
-      <div v-for="(txs, month, index) in txByMonth" :key="`${index}`">
-        <h3 class='tx-list__subheading'>{{month.replace(/[0-9]/g, '')}}</h3>
-        <SpTxListItem v-for='(tx, i) in txs' :key='`${tx.hash}-${tx.timestamp}-${i}`' :tx="tx" />
+          <div class='loading__col loading__col--justify-end'>
+            <span class='loading__amount'></span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div v-else class="empty">Transaction history is empty</div>
-
-    <div
-      v-if="leftToShowMore"
-      class="show-more"
-      role="button"
-      @click="showMoreItems"
-    >
-      Show more
-      <span class='arrow-icon'>
-        <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6.0001 7.4001L0.600098 2.0001L2.0001 0.600098L6.0001 4.6001L10.0001 0.600098L11.4001 2.0001L6.0001 7.4001Z" fill="black"/>
-        </svg>
-      </span>
-    </div>
-  </div>
+    </template>
+  </Suspense>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, reactive } from 'vue'
-import { useStore } from 'vuex'
+<script>
+import SpGetTxList from '../SpGetTxList'
 
-import { useTxs } from '../../composables'
-import { TxForUI } from '../../composables/useTxs'
-import SpSpinner from '../SpSpinner'
-import SpTxListItem from '../SpTxListItem'
-
-export interface State {
-  listSize: number
-  listMaxSize: number
-  isNewTxLoading: boolean
-}
-
-export let initialState: State = {
-  listSize: 10,
-  listMaxSize: 15,
-  isNewTxLoading: false
-}
-
-export default defineComponent({
+export default {
   name: 'SpTxList',
 
-  components: { SpSpinner, SpTxListItem },
-
-  async setup() {
-
-
-    // store
-    let $s = useStore()
-
-    // state
-    let state: State = reactive(initialState)
-
-    // composables
-    let { pager, normalize, newTxs } = await useTxs({
-      $s,
-      opts: { order: 'desc', realTime: true }
-    })
-
-    // computed
-    let list = computed<TxForUI[]>(() => {
-        return pager.value.page.value
-          .map(normalize)
-          .slice(0, state.listSize)
-          .sort((a, b) => b.height - a.height)
-    })
-    let txByMonth = computed(() => {
-      const groupByYear = groupBy("timestamp");
-      return groupByYear(list.value);
-    })
-    let leftToShowMore = computed<boolean>(
-      () =>
-        state.listSize < state.listMaxSize &&
-        pager.value.page.value.length > state.listSize
-    )
-    let showMoreText = computed<string>(
-      () => `${newTxs.value} new ${newTxs.value > 1 ? 'items' : 'item'}`
-    )
-
-    // methods
-    let loadNewItems = async () => {
-      state.isNewTxLoading = true
-      await pager.value.load()
-      state.isNewTxLoading = !!newTxs.value
-    }
-    let showMoreItems = () => {
-      state.listSize = state.listMaxSize
-    }
-    let getTxMonth = (timestamp):string =>
-      new Date(timestamp).toLocaleString('en-US', {
-        year: "numeric",
-        month: 'long'
-      });
-    let groupBy = (key) => (array) =>
-      array.reduce((acc, obj) => {
-        const property = getTxMonth(obj[key]);
-        acc[property] = (acc[property] || []).concat(obj);
-        return acc;
-      }, {});
-
-    return {
-      // state
-      newTxs,
-      // computed
-      list,
-      leftToShowMore,
-      showMoreText,
-      /// methods
-      loadNewItems,
-      showMoreItems,
-      txByMonth,
-      getTxMonth,
-      state,
-    }
-  }
-})
+  components: { SpGetTxList },
+}
 </script>
 
-<style scoped lang="scss">
-.empty {
-  /* Body/M */
+<style lang='scss' scoped>
+$base-color: rgba(0, 0, 0, 0.03);
+$animation-duration: 1.6s;
+$shine-color: rgba(0, 0, 0, 0.06);
+$avatar-offset: 32 + 16;
 
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 150%;
-  /* identical to box height, or 24px */
-
-  /* light/muted */
-
-  color: rgba(0, 0, 0, 0.667);
-}
 .title {
   font-family: Inter, serif;
   font-style: normal;
@@ -161,69 +49,103 @@ export default defineComponent({
   color: #000000;
   margin-bottom: 32px;
 }
-.tx-list {
-  position: relative;
-  padding-bottom: 40px;
 
-  &__subheading {
-    font-family: Inter;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 150%;
-    letter-spacing: -0.007em;
-    color: #000000;
-    margin-bottom: 18px;
+.loading {
+  &__row {
+    box-sizing: border-box;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex: 0 1 auto;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 29px;
+  }
+
+  &__col {
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    flex-basis: 0;
+    max-width: 100%;
+    display: flex;
+    align-items: center;
+
+    &--justify-center {
+      justify-content: center;
+    }
+
+    &--justify-end {
+      justify-content: end;
+    }
+  }
+
+  &__avatar {
+    width: 32px;
+    height: 32px;
+    border-radius: 24px;
+    display: inline-flex;
+
+    background: linear-gradient(90deg, $base-color 0px, $shine-color 40px, $base-color 80px);
+    background-size: 600px;
+    animation: shine-avatar $animation-duration infinite linear
+  }
+
+  &__denom {
+    width: 96px;
+    height: 22px;
+    background: rgba(0, 0, 0, 0.03);
+    border-radius: 4px;
+    margin-left: 16px;
+    display: inline-flex;
+
+    background: linear-gradient(90deg, $base-color 0px, $shine-color 40px, $base-color 80px);
+    background-size: 600px;
+    animation: shine-lines $animation-duration infinite linear
+  }
+
+  &__amount {
+    width: 96px;
+    height: 22px;
+    background: rgba(0, 0, 0, 0.03);
+    border-radius: 4px;
+    margin-left: 16px;
+    display: inline-flex;
+
+    background: linear-gradient(90deg, $base-color 0px, $shine-color 40px, $base-color 80px);
+    background-size: 600px;
+    animation: shine-lines $animation-duration infinite linear
+  }
+
+  &__ibc {
+    width: 64px;
+    height: 22px;
+    background: rgba(0, 0, 0, 0.03);
+    border-radius: 4px;
+    margin-left: 16px;
+    display: inline-flex;
+
+    background: linear-gradient(90deg, $base-color 0px, $shine-color 40px, $base-color 80px);
+    background-size: 600px;
+    animation: shine-lines $animation-duration infinite linear
   }
 }
-.list {
-  display: flex;
-  flex-direction: column;
-}
-.load-more {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0 16px;
-  width: 124px;
-  height: 36px;
-  left: 0;
-  right: 0;
-  top: 68px;
-  background: #ffffff;
-  box-shadow: 3px 9px 32px -4px rgba(0, 0, 0, 0.07);
-  border-radius: 56px;
-  color: #000000;
-  font-weight: 500;
-  font-size: 13px;
-  position: absolute;
-  cursor: pointer;
-  margin: 0 auto;
-  z-index: 8;
-}
-.show-more {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 16px;
-  width: 124px;
-  height: 36px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: #FFFFFF;
-  box-shadow: 3px 9px 32px -4px rgba(0, 0, 0, 0.07);
-  border-radius: 56px;
-  color: #000000;
-  font-weight: 500;
-  font-size: 13px;
-  position: absolute;
-  cursor: pointer;
-  margin: 0 auto;
+
+@keyframes shine-avatar {
+  0% {
+    background-position: -100px + $avatar-offset
+  }
+  40%, 100% {
+    background-position: 140px + $avatar-offset
+  }
 }
 
-.arrow-icon {
-  margin-left: 9px;
+@keyframes shine-lines {
+  0% {
+    background-position: -100px
+  }
+  40%, 100% {
+    background-position: 140px
+  }
 }
 </style>
