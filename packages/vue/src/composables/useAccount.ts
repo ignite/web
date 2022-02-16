13714789d962135619b8f5c60f1275544ca052d8
Arthur Marinho <arthurtutuca@gmail.com>
2022-02-16 14:55:45 -0300

    Create useAccount.ts

---
+++ packages/vue/src/composables/useAccount.ts
@@ -0,0 +1,45 @@
+import { Account, SigningStargateClient } from '@cosmjs/stargate'
+import { EventEmitter } from 'events'
+import { computed, Ref, ref, watch } from 'vue'
+import { Store } from 'vuex'
+import { useAddress } from '.'
+
+type Response = {
+  acc: Ref<Account | undefined>
+}
+
+type Params = {
+  $s: Store<any>
+  opts?: any
+}
+
+export default async function ({ $s }: Params): Promise<Response> {
+  // state
+  let acc = ref<Account | undefined>()
+
+  // composables
+  let { address } = useAddress({ $s })
+
+  // computed
+  let spClient = computed<EventEmitter>(() => $s.getters['common/env/client'])
+  let stargateClient = computed<SigningStargateClient>(
+    () => (spClient.value as any).signingClient as SigningStargateClient
+  )
+
+  //watch
+  watch(
+    () => stargateClient.value,
+    async () => {
+      if (stargateClient.value) {
+        acc.value = (await stargateClient.value.getAccount(
+          address.value
+        )) as Account
+      }
+    },
+    { immediate: true }
+  )
+
+  return {
+    acc
+  }
+}
