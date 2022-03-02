f701f7450712b838f7f529859d4646e3364d259d
marinhoarthur <arthurtutuca@gmail.com>
2022-03-02 17:28:34 -0300

    🏗

--- packages/template/src/main.js
+++ packages/template/src/main.js
@@ -1,9 +1,12 @@
 import starportLibrary from '@starport/vue'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+let pinia = createPinia()
 const app = createApp(App)
-app.use(store).use(router).use(starportLibrary).mount('#app')
+
+app.use(store).use(pinia).use(router).use(starportLibrary).mount('#app')
