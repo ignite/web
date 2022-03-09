978057a6fdbc36893814c3763b92d58c1fffe847
Mihail Marcu <marku.michael@gmail.com>
2022-03-09 16:59:43 +0200

    #216: revert changes for data template

--- packages/template/src/views/Data.vue
+++ packages/template/src/views/Data.vue
@@ -1,15 +1,10 @@
 <template>
   <!-- Uncomment the following component to add a form for a `modelName` -->
-  <SpCrud store-name="foo.bar.bar" item-name="Product" />
-
+  <!-- <SpCrud store-name="org.repo.module" item-name="modelName" /> -->
 </template>
 
 <script>
-import { SpCrud } from '@starport/vue'
-
 export default {
-  name: 'Data',
-
-  components: { SpCrud }
+  name: 'Data'
 }
 </script>
