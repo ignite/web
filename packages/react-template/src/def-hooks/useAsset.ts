f269b87dbc5a9aa1f99c363b4b94cdd3eb515b1c
“Clockwork” <alex@clockwork.gr>
2025-06-26 17:00:23 +0300

    chore: Remove deprecated react lib and template

--- packages/react-template/src/def-hooks/useAsset.ts
+++
@@ -1,9 +0,0 @@
-import useCosmosBankV1Beta1 from "../hooks/useCosmosBankV1Beta1";
-import { useAddressContext } from "./addressContext";
-
-export const useAsset = (denom: string) => {
-  const { address } = useAddressContext();
-  const { QueryBalance } = useCosmosBankV1Beta1();
-  const query = QueryBalance(address, { denom }, {});
-  return { balance: query.data?.balance, isLoading: query.isLoading };
-};
