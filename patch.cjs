fd243b9f682eb7697310a0041d2a871911cdbeb5
“Clockwork” <alex@clockwork.gr>
2025-06-27 09:33:59 +0300

    fix: script typos

--- patch.cjs
+++ patch.cjs
@@ -5,5 +5,5 @@ const templatePkg = require('./packages/vue-template/package.json');
 const bumped = semver.parse(lerna.version, { loose: true }).inc('patch').version;
 templatePkg.devDependencies['@ignt/vue-library'] = `^${bumped}`;
 
-fs.writeFileSync('./pacgages/vue-template/package.json', JSON.stringify(templatePkg, null, 2));
+fs.writeFileSync('./packages/vue-template/package.json', JSON.stringify(templatePkg, null, 2));
 console.log(`Bumped @ignt/vue-library to version ${bumped} in vue-template package.json`);
