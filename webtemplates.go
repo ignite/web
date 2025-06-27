c9bba532698edeb810b2b685c7f0444877f914ca
“Clockwork” <alex@clockwork.gr>
2025-06-27 12:00:33 +0300

    chore: remove react embed

--- webtemplates.go
+++ webtemplates.go
@@ -5,16 +5,11 @@ import (
 	"io/fs"
 )
 
-//go:embed packages/vue-template/* packages/vue-template/** packages/react-template/* packages/react-template/**
+//go:embed packages/vue-template/* packages/vue-template/**
 var webapps embed.FS
 
 // Boilerplate is a vue app starter for Cosmos SDK chains.
 func VueBoilerplate() fs.FS {
 	f, _ := fs.Sub(webapps, "packages/vue-template")
-	return f
-}
-
-func ReactBoilerplate() fs.FS {
-	f, _ := fs.Sub(webapps, "packages/react-template")
 	return f
 }
